19a6dc43f3d856be9d0a34f1d7ce72e1d2058a56
Supachai Yungkeaw <Supachai.Yungkeaw@clbs.co.th>
2016-07-08 18:10:25 +0700

    Internationalization (I18N)

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,24 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+	'language' => 'th',
+    'sourceLanguage' => 'en',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+	    'i18n' => [
+	        'translations' => [
+	            'app*' => [
+	                'class' => 'yii\i18n\PhpMessageSource',
+	                'basePath' => '@common/messages',
+	                'fileMap' => [
+	                    'app' => 'app.php',
+	                    'app/error' => 'error.php',
+	                ],
+	            ],
+	        ],
+	    ],
     ],
     'defaultRoute' => 'market',
     'timeZone' => 'Asia/Bangkok',
---
+++ common/messages/th/app.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Register' => 'สมัครสมาชิก',
+    'Sign in' => 'เข้าสู่ระบบ',
+    'Facebook Login' => 'เข้าสู่ระบบผ่าน facebook',
+];
